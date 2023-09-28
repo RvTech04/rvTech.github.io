75a5c5faa777eadf66f5f83e4837d9e7e5cdfdab
Roy Velasco <rvelas8@my.centennialcollege.ca>
2023-09-27 20:42:35 -0400

    Initial commit.

---
+++ personal-portfoolio/personal-portfoolio/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+res.render('index');
+
+});
+                //PAGES
+            app.get('/about', (reqnpm , res) => {
+                res.render('about');
+                
+                });
+                
+            app.get('/projects', (req, res) => {
+                res.render('projects');
+                
+                });
+                app.get('/contact', (req, res) => {
+                    res.render('contact');
+                    
+                    });
+                    app.get('/services', (req, res) => {
+                        res.render('services');
+                        });
+                        app.get('/Home', (req, res) => {
+                            res.render('Home');
+                            });
+
+                    //FOLDERS
+            app.use(express.static(__dirname + '/css'));
+            app.use(express.static(__dirname + '/Logo'));
+            app.use(express.static(__dirname + '/Documents'));
+            app.use(express.static(__dirname + '/Services Logo'));
+
+
+app.listen(port, () => {
+    console.log('Server is running on port ${port}');
+
+});
